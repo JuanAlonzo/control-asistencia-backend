11e00622d0a9c0dcd3ac5f3cced566c9f7c510ec
Alonso <alojaramillo@outlook.com>
2025-11-04 19:59:20 -0500

    Agregando seguridad con helmet y rateLimit

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import { initDB } from './database.js';
@@ -13,12 +15,18 @@ dotenv.config();
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
 
 let db;
 
+const loginLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+  message: 'Demasiados intentos. Por favor, inténtelo de nuevo más tarde.',
+});
 const PORT = process.env.PORT || 5000;
 const HOST = process.env.HOST || '0.0.0.0';
 
@@ -31,7 +39,7 @@ const HOST = process.env.HOST || '0.0.0.0';
   });
 
   // Routes
-  app.use('/api/auth', authRouter);
+  app.use('/api/auth', loginLimiter, authRouter);
   app.use('/api/usuarios', usersRouter);
   app.use('/api/asistencias', attendancesRouter);
   app.use('/api/reportes', reportsRouter);
